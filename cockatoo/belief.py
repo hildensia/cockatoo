26afd3dc38b740937abc3ac2262f2a791c514002
Johannes Kulick <kulick@hildensia.de>
2015-03-26 17:55:27 +0100

    belief can give you actions

--- cockatoo/belief.py
+++ cockatoo/belief.py
@@ -1,5 +1,7 @@
-from joint_dependency.inference import (model_posterior, prob_locked)
+from joint_dependency.inference import (model_posterior, prob_locked,
+                                        exp_neg_entropy)
 import numpy as np
+from .utils import rand_max
 
 
 class JointDependencyBelief(object):
@@ -46,7 +48,45 @@ class JointDependencyBelief(object):
         locking = self.sample_locking(new_pos)
         return {"data": new_pos, "value": locking[sim_joint]}
 
+    def sample_joint_states(self, n):
+        samples = []
+        for i in range(n):
+            pos = self.pos
 
+            locked = self.sample_locking(self.pos)
 
+            for j, joint in enumerate(self.pos):
+                if not locked[j]:
+                    pos[j] = np.random.randint(0, 180)  # TODO: add
+                                                        # proper limits
 
+            joint = np.random.randint(0, self.pos.shape[0])
+            samples.append((pos, joint))
+        return samples
 
+    def get_best_explore_action(self, n_samples):
+        samples = self.sample_joint_states(n_samples)
+        return rand_max(samples,
+                        key=lambda x: exp_neg_entropy(self.experiences[x[1]],
+                                                      x[0],
+                                                      self.p_same,
+                                                      self.alpha_prior,
+                                                      self.model_prior[x[1]]))
+
+    def get_best_unlock_action(self, n_samples):
+        samples = self.sample_joint_states(n_samples)
+        return rand_max(samples,
+                        key=lambda x: -prob_locked(self.experiences[x[1]],
+                                                   x[0],
+                                                   self.p_same,
+                                                   self.alpha_prior,
+                                                   self.model_prior[x[1]]))
+
+    def get_best_lock_action(self, n_samples):
+        samples = self.sample_joint_states(n_samples)
+        return rand_max(samples,
+                        key=lambda x: prob_locked(self.experiences[x[1]],
+                                                  x[0],
+                                                  self.p_same,
+                                                  self.alpha_prior,
+                                                  self.model_prior[x[1]]))

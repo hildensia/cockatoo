f1f417d7693febeeaea8a0a522ac628ec207be59
Johannes Kulick <kulick@hildensia.de>
2015-03-26 17:56:45 +0100

    add a rand_max function

--- cockatoo/utils.py
+++ cockatoo/utils.py
@@ -1,19 +1,30 @@
+import random
 import numpy as np
 
 
-def nan_helper(y):
-    """Helper to handle indices and logical indices of NaNs.
-
-    Input:
-        - y, 1d numpy array with possible NaNs
-    Output:
-        - nans, logical indices of NaNs
-        - index, a function, with signature indices= index(logical_indices),
-          to convert logical indices of NaNs to 'equivalent' indices
-    Example:
-        >>> # linear interpolation of NaNs
-        >>> nans, x= nan_helper(y)
-        >>> y[nans]= np.interp(x(nans), x(~nans), y[~nans])
+def rand_max(iterable, key=None):
     """
+    A max function that tie breaks randomly instead of first-wins as in
+    built-in max().
+    :param iterable: The container to take the max from
+    :param key: A function to compute tha max from. E.g.:
+      >>> rand_max([-2, 1], key=lambda x:x**2
+      -2
+      If key is None the identity is used.
+    :return: The entry of the iterable which has the maximum value. Tie
+    breaks are random.
+    """
+    if key is None:
+        key = lambda x: x
 
-    return np.isnan(y), lambda z: z.nonzero()[0]
+    max_v = -np.inf
+    max_l = []
+
+    for item, value in zip(iterable, [key(i) for i in iterable]):
+        if value == max_v:
+            max_l.append(item)
+        elif value > max_v:
+            max_l = [item]
+            max_v = value
+
+    return random.choice(max_l)

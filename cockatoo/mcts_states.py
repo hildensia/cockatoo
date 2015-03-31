41ca38ce15ac958d0a005e059f2f341af6b24465
Johannes Kulick <kulick@hildensia.de>
2015-03-31 18:16:07 +0200

    simulator to perform actions

--- cockatoo/mcts_states.py
+++ cockatoo/mcts_states.py
@@ -1,3 +1,4 @@
+from __future__ import division, print_function
 from enum import Enum
 from copy import deepcopy
 import numpy as np
@@ -7,50 +8,116 @@ from scipy.stats import entropy
 from cockatoo.belief import JointDependencyBelief
 
 
-class JointDependencyAction(Enum):
+def ass_bool(obj):
+    assert isinstance(obj, bool) or isinstance(obj, np.bool_)
+
+
+class JointDependencyActionType(Enum):
+
     explore = 0
-    exploit = 1
+    lock = 1
+    unlock = 2
+
+
+class JointDependencyAction(object):
+    def __init__(self, type, joint_idx):
+        self.type = type
+        self.joint_idx = joint_idx
+
+    def __eq__(self, other):
+        return self.joint_idx == other.joint_idx and self.type == other.type
+
+    def __hash__(self):
+        return 100 * self.type.__hash__() + self.joint_idx
+
+    def __str__(self):
+        return self.type.name + " joint " + str(self.joint_idx)
 
 
 class JointDependencyState(object):
-    def __init__(self, belief):
+    def __init__(self, belief, locking, simulator):
         self.performed = {}
         self.belief = belief
+        self.locking = locking
         self.objective_fnc = exp_neg_entropy
         self.n_samples = 1000
+        self.actions = [JointDependencyAction(type, joint)
+                        for type in JointDependencyActionType
+                        for joint in range(belief.pos.shape[0])]
+        self.simulator = simulator
 
     def perform(self, action):
         sim_action = self._get_best_action(action)
-        experience = self.belief.simulate(sim_action)
+        action.move_to = deepcopy(sim_action)
+        locking, new_pos = self.belief.simulate(sim_action, action.joint_idx,
+                                                self.locking)
 
         experiences = deepcopy(self.belief.experiences)
-        experiences.append(experience)
+        for idx, l in enumerate(locking):
+            experiences[idx].append({"data": new_pos, "value": 1 if l else 0})
 
-        belief = JointDependencyBelief(experiences)
-        return JointDependencyState(belief)
+        belief = JointDependencyBelief(new_pos, experiences, self.belief)
+        return JointDependencyState(belief, locking, self.simulator)
+
+    def real_world_perform(self, action):
+        experiences = deepcopy(self.belief.experiences)
+        print(experiences)
+
+        print("LOCKING: {}".format(self.locking))
+        print("ACTION: {}".format(self.performed[action]))
+        self.simulator.action_machine.run_action(self.performed[action])
+
+        q = [joint.get_q() for joint in self.simulator.world.joints]
+        locking = [j.locked
+                   for j in self.simulator.world.joints]
+        for idx, joint in enumerate(self.simulator.world.joints):
+            exp = {"data": q, "value": 1 if joint.locked else 0}
+            experiences[idx].append(exp)
+
+        belief = JointDependencyBelief(q, experiences,
+                                       self.belief)
+        return JointDependencyState(belief, locking, self.simulator)
 
     def _get_best_action(self, action):
         try:
             return self.performed[action]
         except KeyError:
-            if action == JointDependencyAction.explore:
+            if action.type == JointDependencyActionType.explore:
                 self.performed[action] = \
-                    self.belief.get_best_explore_action(self.n_samples)
-            elif action == JointDependencyAction.exploit:
+                    deepcopy(self.belief.get_best_explore_action(self.locking))
+            elif action.type == JointDependencyActionType.unlock:
                 self.performed[action] = \
-                    self.belief.get_best_exploit_action(self.n_samples)
+                    deepcopy(self.belief.get_best_unlock_action(self.n_samples,
+                                                                action.joint_idx,
+                                                                self.locking))
+            elif action.type == JointDependencyActionType.lock:
+                self.performed[action] = \
+                    deepcopy(self.belief.get_best_lock_action(self.n_samples,
+                                                              action.joint_idx,
+                                                              self.locking))
             return self.performed[action]
 
     def reward(self, parent, _):
         open = 0
-        n = 1000
         # estimate the probability of all open
         # (TODO: can we do it bayesian?)
-        for _ in range(n):
-            locking = self.belief.sample_locking(self.belief.pos)
-            if not any(locking):
-                open += 1
-        reward = 100 * (open/n)
-        return reward + entropy(parent.belief.posteriors, self.belief.posteriors)
+        # pl = self.belief.prob_locking(self.belief.pos)
+        # for _ in range(n):
+        #     locking = np.random.uniform() < pl[4].rvs()[0][0]
+        #     if not locking:
+        #         open += 1
+        # reward = 100 * (open/n)
+        reward = sum(entropy(np.array(parent.belief.posteriors).T,
+                             np.array(self.belief.posteriors).T))
+        print(".", end="")
+        return 0
+        return reward
 
+    def is_terminal(self):
+        return False
 
+    def __eq__(self, other):
+        return (self.locking == other.locking).all()
+
+    def __hash__(self):
+        return int(np.sum([l*2**i for i, l in enumerate(self.locking)]))

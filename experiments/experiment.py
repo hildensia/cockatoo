8268c18535b5aa28ead16583f567943c0005fd5c
Johannes Kulick <kulick@hildensia.de>
2015-04-21 14:01:30 +0200

    update experimental stuff

--- experiments/experiment.py
+++ experiments/experiment.py
@@ -1,7 +1,12 @@
 from cockatoo.mcts_states import JointDependencyBelief, JointDependencyState
+from cockatoo.utils import nan_helper
+
 from joint_dependency.inference import same_segment
 from joint_dependency.simulation import (Joint, MultiLocker, Controller,
                                          ActionMachine, World)
+from joint_dependency.recorder import Record
+
+import bayesian_changepoint_detection.offline_changepoint_detection as bcd
 import numpy as np
 import mcts.graph
 import mcts.mcts
@@ -10,6 +15,12 @@ import mcts.tree_policies
 import mcts.default_policies
 
 import cProfile
+import argparse
+import multiprocessing
+from functools import partial
+
+import matplotlib.pyplot as plt
+
 
 class Simulator(object):
     def __init__(self, noise):
@@ -40,7 +51,92 @@ class Simulator(object):
         self.action_machine = ActionMachine(self.world, controllers)
 
 
+def argparsing():
+    parser = argparse.ArgumentParser(description='Process some integers.')
+    parser.add_argument('--intrinsic_motivation', '-i', action='store_true',
+                        help='Use intrinsic motivation')
+    parser.add_argument('--goal_reward', '-g', action='store_true',
+                        help='Give reward on the goal state.')
+    args = parser.parse_args()
+    print("Intrinsic Motivation: {}".format("On" if args.intrinsic_motivation
+                                            else "Off"))
+    print("Goal Reward: {}".format("On" if args.goal_reward
+                                   else "Off"))
+    return args
+
+
+def get_probability_over_degree(P, qs):
+    probs = np.zeros((360,))
+    count = np.zeros((360,))
+    for i, pos in enumerate(qs[:-2]):
+
+        deg = int(pos)%360
+
+        probs[deg] += P[i]
+        count[deg] += 1
+
+    probs = probs/count
+    prior = 10e-8
+    probs = np.array([prior if np.isnan(p) else p for p in probs])
+    return probs, count
+
+
+def compute_p_same(p_cp):
+    p_same = []
+    for pcp in p_cp:
+        p_same.append(same_segment(pcp))
+    return p_same
+
+
+def update_p_cp(world, use_ros):
+    P_cp = []
+    pid = multiprocessing.current_process().pid
+    for j, joint in enumerate(world.joints):
+        if use_ros:
+            q = Record.records[pid]["q_" + str(j)].as_matrix()
+            af = Record.records[pid]["applied_force_" + str(j)][0:].as_matrix()
+            v = q[1:] - q[:-1]  # we can't measure the velocity directly
+
+            vn = v[:] + af[1:]
+            d = np.zeros((v.shape[0] + 1,))
+            d[1:] = abs((vn**2 - v[:]**2)/(0.1 * vn))
+        else:
+            v = Record.records[pid]["v_" + str(j)].as_matrix()
+            af = Record.records[pid]["applied_force_" + str(j)].as_matrix()
+            vn = v + af
+            d = np.zeros(v.shape)
+            d[1:] = (vn[:-1]**2 - v[1:]**2)/(0.1 * vn[:-1])
+
+            saved = 0
+            for i, vel in enumerate(v):
+                if vel == 0:
+                    d[i] = saved
+                else:
+                    saved = d[i]
+
+            d[0] = d[1]
+
+        nans, x = nan_helper(d)
+        if not all(nans):
+            d[nans] = np.interp(x(nans), x(~nans), d[~nans])
+
+        Q, P, Pcp = bcd.offline_changepoint_detection(
+            data=d,
+            prior_func=partial(bcd.const_prior, l=(len(d)+1)),
+            observation_log_likelihood_function=
+            bcd.gaussian_obs_log_likelihood,
+            truncate=-50)
+
+        p_cp, count = get_probability_over_degree(
+            np.exp(Pcp).sum(0)[:],
+            Record.records[pid]['q_' + str(j)][1:].as_matrix())
+
+        P_cp.append(p_cp)
+    return P_cp
+
+
 def main():
+    options = argparsing()
     n = 5
     experiences = [[]]*n
     print(experiences)
@@ -55,8 +151,8 @@ def main():
 
     belief.alpha_prior = np.array([.1, .1])
 
-    p_cp = np.array([.1] * 360)
-    p_cp[160] = .9
+    p_cp = np.array([.3] * 360)
+    #p_cp[160] = .9
 
     belief.p_same = [same_segment(p_cp)] * n
 
@@ -77,7 +173,7 @@ def main():
     belief.model_prior = model_prior
     noise = {'q': 10e-6, 'vel': 10e-6}
     state = JointDependencyState(belief, [False, True, True, True, True],
-                                 Simulator(noise))
+                                 Simulator(noise), options)
 
     print(belief.posteriors)
 
@@ -87,10 +183,15 @@ def main():
                             backup=mcts.backups.Bellman(gamma=.6))
 
     state_node = root
-    for _ in range(100):
-        action = search(state_node, n=10)
+    for _ in range(1):
+        action = search(state_node, n=500)
         state_node = state_node.children[action].sample_state(real_world=True)
+        p_cp = update_p_cp(state_node.state.simulator.world, False)
+        # plt.plot(p_cp[0])
+        # plt.show()
+        state_node.state.belief.p_same = compute_p_same(p_cp)
         state_node.parent = None
+        print(p_cp)
         print(action)
         print(state_node.state.belief.pos)
         print(state_node.state.belief.posteriors)
@@ -98,5 +199,5 @@ def main():
 
 
 if __name__ == '__main__':
-    #cProfile.run("main()")
-    main()
+    cProfile.run("main()")
+    # main()

e73b0b39c78188c5f5d4fa872877fcb4ca00384b
Johannes Kulick <kulick@hildensia.de>
2015-05-11 14:51:27 +0200

    compute CPs from within joint space (and do the projection earlier)

--- experiments/experiment.py
+++ experiments/experiment.py
@@ -25,8 +25,11 @@ import collections
 import datetime
 import random
 import logging
+import matplotlib.pyplot as plt
 
 import os
+
+import scipy.ndimage
 
 try:
     import cPickle as pickle
@@ -36,7 +39,7 @@ except ImportError:
 
 ExperimentData = collections.namedtuple(
     "ExperimentData", "action ll_action pos model_posterior locking_state "
-                      "p_same"
+                      "p_cp qs vs afs"
 )
 
 Metadata = collections.namedtuple(
@@ -44,7 +47,7 @@ Metadata = collections.namedtuple(
 )
 
 class Simulator(object):
-    def __init__(self, noise, num_of_joints):
+    def __init__(self, noise, num_of_joints, tau=.01):
         self.world = World([])
 
         #  master | slave | open | closed
@@ -57,7 +60,6 @@ class Simulator(object):
         limits = (0, 180)
 
         for i in range(num_of_joints):
-
             dampings = [15, 200, 15]
 
             m = random.randint(10, 170)
@@ -80,7 +82,7 @@ class Simulator(object):
 
         controllers = [Controller(self.world, j)
                        for j, _ in enumerate(self.world.joints)]
-        self.action_machine = ActionMachine(self.world, controllers)
+        self.action_machine = ActionMachine(self.world, controllers, tau)
 
 
 def argparsing():
@@ -121,12 +123,29 @@ def argparsing():
 def get_probability_over_degree(P, qs, prior):
     probs = np.zeros((360,))
     count = np.zeros((360,))
-    for i, pos in enumerate(qs[:-2]):
+    deg = 0
+    for i, pos in enumerate(qs):
+        if i > 0:
+            old_deg = deg
 
         deg = int(pos)%360
 
         probs[deg] += P[i]
         count[deg] += 1
+
+        if i > 0 and abs(old_deg-deg) >= 2:
+            bigger = max(deg, old_deg)
+            smaller = min(deg, old_deg)
+            if deg > old_deg:
+                j = i-1
+                k = i
+            else:
+                j = i
+                k = i-1
+            probs[smaller+1:bigger] = np.interp(range(smaller+1, bigger),
+                                                [smaller, bigger],
+                                                [P[j], P[k]])
+            count[smaller+1:bigger] += 1
 
     probs = probs/count
     prior = .01
@@ -141,60 +160,136 @@ def compute_p_same(p_cp):
     return np.array(p_same)
 
 
-def process_update_p_cp(tup):
-    (use_ros, records, j, prior) = tup
-    if use_ros:
-        q = records["q_" + str(j)].as_matrix()
-        af = records["applied_force_" + str(j)][0:].as_matrix()
-        v = q[1:] - q[:-1]  # we can't measure the velocity directly
+def get_damping_over_degree(P, qs):
+    P = np.asarray(P)
+    qs = np.asarray(qs)
+    ds = np.zeros((360,))
+    count = np.zeros((360,))
+    for i, pos in enumerate(qs):
+        deg = int(pos)%360
 
-        vn = v[:] + af[1:]
-        d = np.zeros((v.shape[0] + 1,))
-        d[1:] = abs((vn**2 - v[:]**2)/(0.1 * vn))
-    else:
-        v = records["v_" + str(j)].as_matrix()
-        af = records["applied_force_" + str(j)].as_matrix()
-        vn = v + af
-        d = np.zeros(v.shape)
-        d[1:] = (vn[:-1]**2 - v[1:]**2)/(0.1 * vn[:-1])
+        ds[deg] += P[i]
+        count[deg] += 1
 
-        saved = 0
-        for i, vel in enumerate(v):
-            if vel == 0:
-                d[i] = saved
-            else:
-                saved = d[i]
+    ds = ds/count
+    old_d = 0
+    for i, d in enumerate(ds):
+        if np.isnan(d):
+            ds[i] = old_d
+        else:
+            old_d = d
 
-        d[0] = d[1]
+    return ds, count
+
+
+def compute_damping(v, af, tau):
+    v = np.asarray(v)
+    af = np.asarray(af)
+    vn = v + af
+    d = np.zeros(v.shape)
+    d[1:] = abs(((vn[:-1]**2) - (v[1:]**2))/(tau * vn[:-1]))
+
+    saved = 0
+    for i, vel in enumerate(v):
+        if vel == 0:
+            d[i] = saved
+        else:
+            saved = d[i]
+
+    d[0] = d[1]
 
     nans, x = nan_helper(d)
     if not all(nans):
         d[nans] = np.interp(x(nans), x(~nans), d[~nans])
 
+    return d
+
+
+# def process_update_p_cp(tup):
+#     (use_ros, records, j, prior) = tup
+#     if use_ros:
+#         q = records["q_" + str(j)].as_matrix()
+#         af = records["applied_force_" + str(j)][0:].as_matrix()
+#         v = q[1:] - q[:-1]  # we can't measure the velocity directly
+#
+#         vn = v[:] + af[1:]
+#         d = np.zeros((v.shape[0] + 1,))
+#         d[1:] = abs((vn**2 - v[:]**2)/(0.1 * vn))
+#     else:
+#         v = records["v_" + str(j)].as_matrix()
+#         af = records["applied_force_" + str(j)].as_matrix()
+#         vn = v + af
+#         d = np.zeros(v.shape)
+#         d[1:] = abs((vn[:-1]**2 - v[1:]**2)/(0.1 * vn[:-1]))
+#
+#         saved = 0
+#         for i, vel in enumerate(v):
+#             if vel == 0:
+#                 d[i] = saved
+#             else:
+#                 saved = d[i]
+#
+#         d[0] = d[1]
+#
+#     nans, x = nan_helper(d)
+#     if not all(nans):
+#         d[nans] = np.interp(x(nans), x(~nans), d[~nans])
+#
+#     Q, P, Pcp = bcd.offline_changepoint_detection(
+#         data=d,
+#         prior_func=partial(bcd.const_prior, l=(len(d)+1)),
+#         observation_log_likelihood_function=
+#         bcd.gaussian_obs_log_likelihood,
+#         truncate=-50)
+#
+#     p_cp, count = get_probability_over_degree(
+#         np.exp(Pcp).sum(0)[:],
+#         records['q_' + str(j)][1:].as_matrix(),
+#         prior
+#     )
+#
+#     return p_cp
+
+
+def process_cp(d):
     Q, P, Pcp = bcd.offline_changepoint_detection(
         data=d,
         prior_func=partial(bcd.const_prior, l=(len(d)+1)),
         observation_log_likelihood_function=
         bcd.gaussian_obs_log_likelihood,
-        truncate=-50)
+        truncate=-len(d)/2.3)
 
-    p_cp, count = get_probability_over_degree(
-        np.exp(Pcp).sum(0)[:],
-        records['q_' + str(j)][1:].as_matrix(),
-        prior
-    )
-
-    return p_cp
+    return np.exp(Pcp).sum(0)
 
 
-def update_p_cp(world, use_ros, prior, pool):
+def process_update_p_cp(tup):
+    use_ros, records, j = tup
+    q = records["q_" + str(j)].as_matrix()
+    if use_ros:
+        af = records["applied_force_" + str(j)].as_matrix()[1:]
+        v = q[1:] - q[:-1]  # we can't measure the velocity directly
+    else:
+        v = records["v_" + str(j)].as_matrix()
+        af = records["applied_force_" + str(j)].as_matrix()
+
+    damping = compute_damping(v, af, .01)
+    damping_deg, counts = get_damping_over_degree(damping, q)
+
+    pcpd = process_cp(damping_deg)
+    pcpd[counts[1:] == 0] = np.maximum(.01, pcpd[counts[1:] == 0])
+
+    pcpd = scipy.ndimage.filters.gaussian_filter1d(pcpd, 1)*2
+
+    return pcpd
+
+
+def update_p_cp(world, use_ros, pool):
     pid = multiprocessing.current_process().pid
     data = zip([use_ros] * len(world.joints),
                [Record.records[pid]] * len(world.joints),
-               range(len(world.joints)),
-               prior)
+               range(len(world.joints)))
 
-    return pool.map(process_update_p_cp, data)
+    return pool.map(process_update_p_cp, data), Record.records[pid]
 
 
 def init_model_prior(n):
@@ -270,8 +365,13 @@ def main():
 
     # setup container for experiment data
     data = []
-    filename = os.path.join(options.output_directory, 'cockatoo_{}.pkl'.format(
-        datetime.datetime.now().strftime("%y%m%d%H%M%S")))
+    filename = os.path.join(
+        options.output_directory,
+        'cockatoo_{}_{}.pkl'.format(
+            datetime.datetime.now().strftime("%y%m%d%H%M%S"),
+            options.seed))
+
+    print("Save to {}".format(filename))
 
     for _ in range(options.action_n):
         # reset multiprocessing pool
@@ -288,13 +388,16 @@ def main():
         state_node = state_node.children[action].sample_state(real_world=True)
 
         # update change point probabilities from force sensor data
-        p_cp = update_p_cp(state_node.state.simulator.world, False, prior=p_cp,
-                           pool=pool)
+        p_cp, records = update_p_cp(state_node.state.simulator.world, False,
+                                    pool=pool)
         JointDependencyBelief.p_same = compute_p_same(p_cp)
 
-        # for pcp in p_cp:
-        #     plt.plot(pcp)
-        # plt.show()
+        for i, pcp in enumerate(p_cp):
+            plt.plot(pcp, label="{}".format(i))
+        plt.legend()
+        plt.grid()
+        plt.xticks(np.linspace(0,100,51))
+        plt.show()
 
         # set best state as new root node
         state_node.parent = None
@@ -305,6 +408,10 @@ def main():
             np.asarray(state_node.state.belief.posteriors)))
         print("Locking state: {}".format(state_node.state.locking))
 
+        qs = [records['q_{}'.format(q)] for q in range(number_of_joints)]
+        vs = [records['v_{}'.format(q)] for q in range(number_of_joints)]
+        afs = [records['applied_force_{}'.format(q)] for q in range(number_of_joints)]
+
         # pack data for later saving
         expd = ExperimentData(
             action=action,
@@ -312,7 +419,10 @@ def main():
             pos=state_node.state.belief.pos,
             model_posterior=state_node.state.belief.posteriors,
             locking_state=state_node.state.locking,
-            p_same=np.array(JointDependencyBelief.p_same))
+            p_cp=np.array(p_cp),
+            qs=qs,
+            vs=vs,
+            afs=afs)
         data.append(expd)
 
         # shutdown workers from pool
@@ -324,5 +434,4 @@ def main():
             pickle.dump((meta, data), f)
 
 if __name__ == '__main__':
-    cProfile.run("main()")
-    # main()
+    main()
